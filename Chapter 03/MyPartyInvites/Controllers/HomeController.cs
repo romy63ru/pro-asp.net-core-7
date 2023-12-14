921bfa856d620adc3b1d47c959a0c2a64ee0cc80
Alex Romanov <romy63ru@hotmail.com>
2023-12-14 20:51:56 +0100

    Partial realisation of project

--- Chapter 03/MyPartyInvites/Controllers/HomeController.cs
+++ Chapter 03/MyPartyInvites/Controllers/HomeController.cs
@@ -1,31 +1,25 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyPartyInvites.Models;
 
 namespace MyPartyInvites.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpGet]
+    public ViewResult RsvpForm()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        Repository.AddResponse(guestResponse);
+        return View("Thank", guestResponse);
     }
 }
