31267f498432d2cf5fd9f4c2184e7d92eb996a04
Alex Romanov <romy63ru@hotmail.com>
2023-12-14 19:52:59 +0100

    project init

---
+++ Chapter 03/MyPartyInvites/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyPartyInvites.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Chapter 03/init.bash
@@ -0,0 +1,7 @@
+dotnet new globaljson --sdk-version 7.0.100 --output MyPartyInvites
+dotnet new mvc --no-https --output MyPartyInvites --framework net7.0
+dotnet new sln -o MyPartyInvites
+dotnet sln MyPartyInvites add MyPartyInvites
+cd MyPartyInvites
+dotnet new gitignore
+
